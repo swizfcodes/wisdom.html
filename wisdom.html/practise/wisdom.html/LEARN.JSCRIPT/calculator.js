bf05a1f1deeeca253b38c705fcab5abbe58caf00
swizfcodes <elishagodwin897@gmail.com>
2024-12-10 16:09:17 +0100

    Add files via upload

---
+++ wisdom.html/practise/wisdom.html/LEARN.JSCRIPT/calculator.js
@@ -0,0 +1,19 @@
+//JavaScript for calculator
+
+//This code helps us 
+const display = document.getElementById('display');
+
+function appendToDisplay(input){
+    display.value += input;
+}
+
+function clearDisplay(){
+    display.value = '';
+}
+
+function calculate(){
+    try{ display.value = eval(display.value);
+    } catch(error){
+        display.value = 'error';
+    }
+}
---
+++ wisdom.html/practise/wisdom.html/cart/cart.js
@@ -0,0 +1,2 @@
+let cartQuantity = 0;
+
